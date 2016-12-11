168108788b31ab0361dbb6430dcf28a865752d54
Eva Lieu <itsevalieu@gmail.com>
2016-12-11 05:45:22 -0800

    Create .gitignore file; CLI.js includes inquirer interface

--- CLI.js
+++ CLI.js
@@ -1,1 +1,24 @@
 var inquirer = require("inquirer");
+
+inquirer.prompt([
+	{
+		name: "name",
+		message: "What's your name?"
+	},
+	{
+		name: "action",
+		message: "What would you like to do with the flashcards?",
+		type: "list",
+		choices: [
+			"create",
+			"review"
+		]
+	}
+	]).then(function(answers){
+		console.log("Your name is: " + answers.name);
+		console.log("Action: " + answers.action);
+		
+		if(answers.action === "create"){
+			console.log("Going to create either a basic or cloze flashcard.");
+		}
+});
